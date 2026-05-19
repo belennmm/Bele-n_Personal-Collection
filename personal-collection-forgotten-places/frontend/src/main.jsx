437762e5c42a397679e8c9b8b629451c631596f1
Belen Monterroso <belenmonterroso@gmail.com>
2026-05-18 22:09:40 -0600

    agregar referencias a main

--- personal-collection-forgotten-places/frontend/src/main.jsx
+++ personal-collection-forgotten-places/frontend/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import StorageProvider from './context/StorageProvider.jsx'
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <StorageProvider>
+      <App />
+    </StorageProvider>
+   
   </StrictMode>,
 )
