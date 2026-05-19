0bd0e688028df71b34e8d2e26755e1f6b04c17b8
Belen Monterroso <belenmonterroso@gmail.com>
2026-05-18 23:01:16 -0600

    arreglar conexión con backend

--- personal-collection-forgotten-places/frontend/src/context/StorageProvider.jsx
+++ personal-collection-forgotten-places/frontend/src/context/StorageProvider.jsx
@@ -9,6 +9,7 @@ function StorageProvider({ children }){
   const [error , setError ] = useState(null) ;
 
   const API_URL = import.meta.env.VITE_API_URL  || "http://localhost:3001" ;
+  console.log("API_URL:" , API_URL) ;
 
   function setModo(nuevoModo){
     setModoState(nuevoModo) ;
@@ -172,7 +173,7 @@ function StorageProvider({ children }){
       {children}
     </StorageContext.Provider>
   ) ;
-  
+
 }
 
 export default StorageProvider ;
